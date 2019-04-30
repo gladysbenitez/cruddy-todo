c0122ace41b5aecb035b073c7e0c18375837db22
gladysbenitez <gladysbenitez.an@gmail.com>
2019-04-30 12:12:37 -0500

    compelte delete function

--- datastore/index.js
+++ datastore/index.js
@@ -83,14 +83,18 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fileEnd = id + ".txt";
+  file = path.join(exports.dataDir, fileEnd);
+  fs.unlink(file,callback);
+
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
